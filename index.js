dc4679b174799ab55672d9233bed6075bf99a34e
Felipe <alves.f.felipe02@gmail.com>
2021-10-03 20:59:27 -0300

    terceiro

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const path = require("path");
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded());
-S
+
 let message = "";
 
 const pokedex = [{ numero: 1, nome: "Bulbasaur", tipo: "Grama", imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png", descricao: "Há uma semente de planta em suas costas desde o dia em que o Pokémon nasceu. A semente cresce lentamente.", altura: 0.7, peso: 6.9, categoria: "Semente", habilidade: "Overgrow"}, { numero: 2, nome: "Charmander", tipo: "Fogo", imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png", descricao: "Tem preferência por coisas quentes. Quando chove, diz-se que o vapor jorra da ponta da cauda.", altura: 0.6, peso: 8.5, categoria: "Lagarto", habilidade: "Chama"}, { numero: 3, nome: "Squirtle", tipo: "Água", imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png", descricao: "Quando ele retrai seu longo pescoço em sua concha, ele esguicha água com força vigorosa.", altura: 0.5, peso: 9.0, categoria: "Tartaruga", habilidade: "Torrente"}];
